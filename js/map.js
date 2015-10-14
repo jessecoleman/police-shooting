2a5a7b17943d60da63bfbc8c1d00bb408a3d9175
Jesse Chamberlin <colechamberlin@gmail.com>
2015-10-14 15:06:50 -0700

    fixed reference

--- js/map.js
+++ js/map.js
@@ -2,7 +2,7 @@
 var drawMap = function() {
 
   // Create map and set view
-  var map = L.map('mapContainer').setView([latitude, longitude], center);
+  var map = L.map('container').setView([latitude, longitude], center);
 
   // Create a tile layer variable using the appropriate url
   var layer = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png');
