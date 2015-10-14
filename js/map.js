9c35c770971f61d9406cd858e45db0ccf42a1593
Jesse Chamberlin <colechamberlin@gmail.com>
2015-10-14 14:59:24 -0700

    first commit

--- js/map.js
+++ js/map.js
@@ -2,23 +2,23 @@
 var drawMap() = function() {
 
   // Create map and set view
- 
+  var map = L.map('mapContainer').setView([latitude, longitude], center);
 
   // Create a tile layer variable using the appropriate url
-
+  var layer = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png');
 
   // Add the layer to your map
- 
+  layer.addTo(map);
 
   // Execute your function to get data
- 
+  getData();
 }
 
 // Function for getting data
 var getData = function() {
 
   // Execute an AJAX request to get the data in data/response.js
-
+  $.get('../data/response.json', customBuild());
 
   // When your request is successful, call your customBuild function
 
@@ -26,10 +26,26 @@ var getData = function() {
 
 // Loop through your data and add the appropriate layers and points
 var customBuild = function() {
-	// Be sure to add each layer to the map
+	//gender
+	var layerMale = new L.LayerGroup([]);
+	var layerFemale = new L.LayerGroup([]);
+	this.forEach(processPoints);
 
+	// Be sure to add each layer to the map
+	layerMale.addTo(map);
+	layerMale.addTo(map);
 	// Once layers are on the map, add a leaflet controller that shows/hides layers
-  
+  	L.control.layers(null, layers).addTo(map);
 }
 
+function processPoints(element, index, array) {
+	var circle = new L.circleMarker([element.lat, element.long]).bindPoput(element.Summary)
+	if(element.Gender == "Male") {
+		circle.color = "blue";
+		circle.addTo(layerMale);
+	} else {
+		circle.color = "red";
+		circle.addTo(layerFemale);
+	}
+}
 
