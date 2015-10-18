c1e04cb6c877e8a61d3b6758eb4ad80a153a51f6
Jesse Chamberlin <colechamberlin@gmail.com>
2015-10-17 22:11:46 -0700

    linked table rows with map control

--- js/map.js
+++ js/map.js
@@ -1,7 +1,5 @@
 //(function($, L) {
 
-	L.Control.ActiveLayers=L.Control.Layers.extend({getActiveBaseLayer:function(){return this._activeBaseLayer},getActiveOverlayLayers:function(){return this._activeOverlayLayers},onAdd:function(a){var b=L.Control.Layers.prototype.onAdd.call(this,a);return this._activeBaseLayer=this._findActiveBaseLayer(),this._activeOverlayLayers=this._findActiveOverlayLayers(),b},_findActiveBaseLayer:function(){var a=this._layers;for(var b in a)if(this._layers.hasOwnProperty(b)){var c=a[b];if(!c.overlay&&this._map.hasLayer(c.layer))return c}throw new Error("Control doesn't have any active base layer!")},_findActiveOverlayLayers:function(){var a={},b=this._layers;for(var c in b)if(this._layers.hasOwnProperty(c)){var d=b[c];d.overlay&&this._map.hasLayer(d.layer)&&(a[c]=d)}return a},_onLayerChange:function(){L.Control.Layers.prototype._onLayerChange.apply(this,arguments),this._recountLayers()},_onInputClick:function(){this._handlingClick=!0,this._recountLayers(),L.Control.Layers.prototype._onInputClick.call(this),this._handlingClick=!1},_recountLayers:function(){var a,b,c,d=this._form.getElementsByTagName("input"),e=d.length;for(a=0;e>a;a++)b=d[a],c=this._layers[b.layerId],b.checked&&!this._map.hasLayer(c.layer)?c.overlay?this._activeOverlayLayers[b.layerId]=c:this._activeBaseLayer=c:!b.checked&&this._map.hasLayer(c.layer)&&c.overlay&&delete this._activeOverlayLayers[b.layerId]}}),L.control.activeLayers=function(a,b,c){return new L.Control.ActiveLayers(a,b,c)};
-
 	//"use strict"
 	
 	//object that displays map
@@ -38,39 +36,35 @@
 		  		//creates base layers for each category
 		  		buildControls(dat);
 		  		//builds overlay layers for specified base layer
-		  		updateOverlays(dat);
+		  		buildMap(dat, "Race");
 		  		//add controls to map
 				var control = L.control.layers(baseMaps, overlayMaps).addTo(map);
 		  		//create table
-		  		buildTable(dat);
+		  		buildTable(dat, "Victim's Gender");
 		  		//add listener to controls
 		  		map.on('baselayerchange', function() {
-		  			console.log(control.getActiveBaseLayer());
+		  			//currently selected base layer
+		  			var selected = $('input[type="radio"]:checked').next()[0].innerHTML.trim();
+		  			buildMap(dat, selected);
+		  			buildTable(dat, selected);
 		  		});
 		  	},
 		  	dataType:'json'
 		});
 	};
 
-	
 	//creates base maps for each category
-	var buildControls = function(data) {
+	var buildControls = function(data_points) {
 		//categories that can be selected for
-		var overlays = ["Victim's Gender", "Victim's Age", "Hit or Killed?", "Armed or Unarmed?", "Weapon"];
+		var overlays = ["Victim's Gender", "Race", "Hit or Killed?", "Armed or Unarmed?"];
 		for(var i = 0; i < overlays.length; i++) {
 			baseMaps[overlays[i]] = new L.LayerGroup([]);
 		}
 	};
 
-	//Unfinished, dynamically update overlay layers based on base layers
-	var updateOverlays = function(data_points) {
-		var selected = null;
-		buildMap(data_points, selected);
-		//return $('input[type="radio"]:checked').name;
-	}
-
 	// Loop through data and add appropriate layers and points
 	var buildMap = function(data_points, selected) {
+
 		//object to store layers names paired with their layerGroup arrays
 		//{"name of layer": L.LayerGroup[..,incident,..], ..}
 		overlayMaps["Unknown"] = new L.LayerGroup([]); //initialize unkown category
@@ -78,7 +72,7 @@
 
 		//iterate through incidents in JSON
 		for(var incident in data_points) {
-			var characteristic = data_points[incident]["Victim's Gender"], //set layers categories
+			var characteristic = data_points[incident][selected], //set layers categories
 			lat = data_points[incident].lat,
 			lng = data_points[incident].lng,
 			smry = data_points[incident].Summary +
@@ -115,14 +109,16 @@
 	};
 
 	//builds cross-tabulated data-structure and then puts its data into table
-	var buildTable = function(data_points) {
+	var buildTable = function(data_points, selected) {
+		//clears out table before reloading it
+		document.getElementById('table').innerHTML = "";
 		//table = {Male: {Black: 30, White: 20...}, Female: {Black: 20, White: 30}}
 		//initialize unknown categories for rows and columns
 		var table = {"Unknown": {"Unknown": 0}};
 
 		for(var incident in data_points) {
 			var row = data_points[incident]["Victim's Gender"]; //set rows
-			var col = data_points[incident]["Race"]; //set columns
+			var col = data_points[incident][selected]; //set columns
 			
 			//if victim's gender is defined
 			if(row) {
@@ -146,7 +142,9 @@
 		//create table header
 		var header = document.createElement('tr');
 		//empty row1 col1
-		header.innerHTML += ('<th>Race</th>');
+		var colHeader = document.createElement('th');
+		colHeader.innerHTML = selected;
+		header.appendChild(colHeader);
 		//set containing all category rows
 		var rows = new Set();
 		//create 
